8da1a99e76f8c95053f320a5c4118acb72233cd9
Kru3en <gorodni4uk.igor@mail.com>
2024-09-13 22:10:21 +0300

    Feat: check A6 task

---
+++ slider.js
@@ -0,0 +1,61 @@
+let currentSlide = 0
+const slides = document.querySelectorAll('.card')
+const totalSlides = slides.length
+const dots = document.querySelectorAll('.dot')
+
+const intervalTime = 500000
+let autoSlideInterval
+
+const slideAnimation = document.querySelector('.slide-animation')
+
+function initializeSlider() {
+  if (window.innerWidth <= 768) {
+    showSlide(currentSlide)
+    resetAutoSlide()
+  } else {
+    clearInterval(autoSlideInterval)
+    slideAnimation.style.transform = `translateX(0%)`
+  }
+}
+
+function showSlide(index, step) {
+  if (step !== undefined) {
+    currentSlide = (currentSlide + step + totalSlides) % totalSlides
+  } else {
+    currentSlide = index
+  }
+
+  if (window.innerWidth <= 320) {
+    offset = -currentSlide * 66
+  } else {
+    offset = -currentSlide * 49
+  }
+  document.querySelector('.row-slider').style.transform =
+    `translateX(${offset}%)`
+
+  dots.forEach(dot => dot.classList.remove('active'))
+  dots[currentSlide % dots.length].classList.add('active')
+}
+
+function moveSlide(step) {
+  showSlide(currentSlide + step, step)
+}
+
+function resetAutoSlide() {
+  clearInterval(autoSlideInterval)
+  autoSlideInterval = setInterval(() => {
+    moveSlide(1)
+  }, intervalTime)
+}
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    if (window.innerWidth <= 768) {
+      showSlide(index)
+      resetAutoSlide()
+    }
+  })
+})
+
+initializeSlider()
+window.addEventListener('resize', initializeSlider)
